16616783d9c4bd08928d484d12f7ae27947cd18c
Redwarp <redwarp@gmail.com>
2020-05-05 19:11:35 +0200

    Naive implementation of matrix scale and offset

--- app/src/main/java/net/redwarp/gifwallpaper/GifDrawer.kt
+++ app/src/main/java/net/redwarp/gifwallpaper/GifDrawer.kt
@@ -2,22 +2,26 @@ package net.redwarp.gifwallpaper
 
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Matrix
 import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.RectF
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.view.SurfaceHolder
-import androidx.core.graphics.withSave
+import android.widget.ImageView
+import androidx.core.graphics.withMatrix
 import kotlin.properties.Delegates
 
 private const val MESSAGE_DRAW = 1
 
 class GifDrawer(private val holder: SurfaceHolder) : SurfaceHolder.Callback2 {
+    var scaleType: ImageView.ScaleType = ImageView.ScaleType.FIT_CENTER
 
-    private var dimensions = Rect(0, 0, 1, 1)
+    private val canvasRect = RectF(0f, 0f, 1f, 1f)
+    private val gifRect = RectF(0f, 0f, 0f, 0f)
     private val emptyPaint = Paint().apply {
         color = Color.GREEN
         style = Paint.Style.FILL
@@ -25,13 +29,20 @@ class GifDrawer(private val holder: SurfaceHolder) : SurfaceHolder.Callback2 {
     private var isCreated = false
     private val handler: Handler = DrawHandler(this)
 
-    var gif: Gif? by Delegates.observable<Gif?>(null) { property, oldValue, newValue ->
+    var gif: Gif? by Delegates.observable(null as Gif?) { _, oldValue, newValue ->
         oldValue?.cleanup()
 
         newValue?.drawable?.callback = drawableCallback
         newValue?.animatable?.start()
+        if (newValue == null) {
+            gifRect.set(0f, 0f, 0f, 0f)
+        } else {
+            gifRect.right = newValue.drawable.intrinsicWidth.toFloat()
+            gifRect.bottom = newValue.drawable.intrinsicHeight.toFloat()
+        }
         invalidate()
     }
+    private val matrix = Matrix()
 
     init {
         holder.addCallback(this)
@@ -42,8 +53,8 @@ class GifDrawer(private val holder: SurfaceHolder) : SurfaceHolder.Callback2 {
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        dimensions.right = width
-        dimensions.bottom = height
+        canvasRect.right = width.toFloat()
+        canvasRect.bottom = height.toFloat()
 
         surfaceRedrawNeeded(holder)
     }
@@ -57,8 +68,45 @@ class GifDrawer(private val holder: SurfaceHolder) : SurfaceHolder.Callback2 {
         handler.removeMessages(MESSAGE_DRAW)
     }
 
+    private fun computeMatrix(
+        matrix: Matrix,
+        scaleType: ImageView.ScaleType,
+        canvasRect: RectF,
+        gifRect: RectF
+    ) {
+        when (scaleType) {
+            ImageView.ScaleType.FIT_CENTER ->
+                matrix.setRectToRect(gifRect, canvasRect, Matrix.ScaleToFit.CENTER)
+        }
+    }
+
+    private fun computeMatrix(matrix: Matrix, canvasRect: RectF, gifRect: RectF) {
+        val canvasRatio = canvasRect.width() / canvasRect.height()
+        val gifRatio = gifRect.width() / gifRect.height()
+
+
+        with(matrix) {
+            reset()
+            if (gifRatio > canvasRatio) {
+                val scale = canvasRect.width() / gifRect.width()
+                val offset = (canvasRect.height() - gifRect.height() * scale) * 0.5f
+
+                postScale(scale, scale)
+                postTranslate(0f, offset)
+            } else {
+
+                val scale = canvasRect.height() / gifRect.height()
+                val offset = (canvasRect.width() - gifRect.width() * scale) * 0.5f
+
+                postScale(scale, scale)
+                postTranslate(offset, 0f)
+            }
+        }
+    }
+
     private fun invalidate() {
         handler.removeMessages(MESSAGE_DRAW)
+        computeMatrix(matrix, scaleType, canvasRect, gifRect)
         handler.sendMessage(getDrawMessage(gif))
     }
 
@@ -87,24 +135,27 @@ class GifDrawer(private val holder: SurfaceHolder) : SurfaceHolder.Callback2 {
     }
 
     private fun drawGif(canvas: Canvas, gif: Gif) {
-        canvas.drawRect(dimensions, gif.backgroundPaint)
+        canvas.drawRect(canvasRect, gif.backgroundPaint)
 
-        canvas.withSave {
-            // Adjust size and position so that
-            // the image looks good on your screen
-            val scale =
-                width.toFloat() / gif.drawable.intrinsicWidth.toFloat()
-            val offset =
-                (height / scale - gif.drawable.intrinsicHeight) * 0.5f
-
-            scale(scale, scale)
-            translate(0f, offset)
+        canvas.withMatrix(matrix) {
             gif.drawable.draw(this)
         }
+        // canvas.withSave {
+        //     // Adjust size and position so that
+        //     // the image looks good on your screen
+        //     val scale =
+        //         width.toFloat() / gif.drawable.intrinsicWidth.toFloat()
+        //     val offset =
+        //         (height / scale - gif.drawable.intrinsicHeight) * 0.5f
+        //
+        //     scale(scale, scale)
+        //     translate(0f, offset)
+        //     gif.drawable.draw(this)
+        // }
     }
 
     private fun drawEmptySurface(canvas: Canvas) {
-        canvas.drawRect(dimensions, emptyPaint)
+        canvas.drawRect(canvasRect, emptyPaint)
     }
 
     private fun getDrawMessage(gif: Gif?): Message {

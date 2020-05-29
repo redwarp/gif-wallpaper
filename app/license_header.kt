5383641bf74f8a5809cda28352260fca4745ec88
Redwarp <redwarp@gmail.com>
2020-05-29 11:02:26 +0200

    Apply extended license to all files

--- app/license_header.kt
+++ app/license_header.kt
@@ -13,11 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.redwarp.gifwallpaper.data
-
-import androidx.annotation.ColorInt
-import androidx.palette.graphics.Palette
-
-sealed class ColorInfo
-object NotSet : ColorInfo()
-class ColorScheme(@ColorInt val defaultColor: Int, val palette: Palette) : ColorInfo()

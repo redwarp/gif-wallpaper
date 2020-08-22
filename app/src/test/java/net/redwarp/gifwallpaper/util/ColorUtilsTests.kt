97bbd7711b2bdcd4a47c87f656be723e9a29122d
Redwarp <redwarp@gmail.com>
2020-08-22 12:07:42 +0200

    Setup robolectric and 2 stupid tests

---
+++ app/src/test/java/net/redwarp/gifwallpaper/util/ColorUtilsTests.kt
@@ -0,0 +1,36 @@
+/* Copyright 2020 Redwarp
+ * Copyright 2020 GifWallpaper Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.redwarp.gifwallpaper.util
+
+import android.graphics.Color
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ColorUtilsTests {
+
+    @Test
+    fun isDark_black_returnsTrue() {
+        Assert.assertTrue(Color.BLACK.isDark())
+    }
+
+    @Test
+    fun isDark_white_returnsFalse() {
+        Assert.assertFalse(Color.WHITE.isDark())
+    }
+}

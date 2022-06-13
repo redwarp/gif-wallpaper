3161e7581621db3ea6eb1d5be4cf731ca62b4844
Benoit Vermont <redwarp@gmail.com>
2022-06-13 18:46:15 +0200

    chore: Update pre-commit hooks

--- app/src/main/java/net/redwarp/gifwallpaper/renderer/SurfaceDrawableRenderer.kt
+++ app/src/main/java/net/redwarp/gifwallpaper/renderer/SurfaceDrawableRenderer.kt
@@ -22,7 +22,6 @@ import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
-import android.os.SystemClock
 import android.util.Log
 import android.view.Surface
 import android.view.SurfaceHolder

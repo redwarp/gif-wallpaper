6dd8509805b6be038432e414d7037e6a2d8072ed
Redwarp <redwarp@gmail.com>
2022-07-16 15:35:49 +0200

    feat!: Update version code in gradle file

--- release/src/main.rs
+++ release/src/main.rs
@@ -1,4 +1,7 @@
 use std::fmt::Display;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
 use std::path::PathBuf;
 
 use anyhow::anyhow;
@@ -6,8 +9,12 @@ use anyhow::Ok;
 use anyhow::Result;
 use conventional_commits_next_version_lib::Commits;
 use git2::Repository;
+use regex::Captures;
+use regex::Regex;
 
 const PROJECT_DIR: &str = env!("CARGO_MANIFEST_DIR");
+const VERSION_NAME_REGEX: &str = r#"(versionName ")([0-9]+\.[0-9]+\.[0-9]+)(")"#;
+const VERSION_CODE_REGEX: &str = r#"(versionCode )([0-9]+)"#;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 struct Version {
@@ -35,17 +42,8 @@ impl Version {
     }
 
     fn as_tag(&self) -> String {
-        format!("{self}")
+        format!("v{self}")
     }
-
-    // fn as_string(&self) -> String {
-    //     format!(
-    //         "{major}.{minor}.{patch}",
-    //         major = self.major,
-    //         minor = self.minor,
-    //         patch = self.patch
-    //     )
-    // }
 
     fn as_semver(&self) -> semver::Version {
         semver::Version::new(self.major, self.minor, self.patch)
@@ -63,7 +61,7 @@ impl Version {
 impl Display for Version {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!(
-            "v{major}.{minor}.{patch}",
+            "{major}.{minor}.{patch}",
             major = self.major,
             minor = self.minor,
             patch = self.patch
@@ -75,7 +73,7 @@ fn main() -> Result<()> {
     let repository =
         Repository::open(PathBuf::from(PROJECT_DIR).join("..")).expect("Couldn't find git repo");
 
-    let last_version = latest_version(&repository)?;
+    let last_version = last_version(&repository)?;
 
     println!("Latest version: {last_version}");
 
@@ -83,10 +81,12 @@ fn main() -> Result<()> {
 
     println!("Next version: {next_version}");
 
+    update_version_in_build_gradle(&next_version)?;
+
     Ok(())
 }
 
-fn latest_version(repository: &Repository) -> Result<Version> {
+fn last_version(repository: &Repository) -> Result<Version> {
     let tags: Result<Vec<Version>> = repository
         .tag_names(None)
         .expect("Couldn't get tags")
@@ -115,4 +115,35 @@ fn next_version(repository: &Repository, last_version: &Version) -> Result<Versi
     );
 
     Ok(Version::from_semver(&version))
+}
+
+fn update_version_in_build_gradle(next_version: &Version) -> Result<()> {
+    let app_gradle_file = PathBuf::from(PROJECT_DIR)
+        .join("..")
+        .join("app/build.gradle");
+
+    let mut content = String::new();
+    File::open(&app_gradle_file)?.read_to_string(&mut content)?;
+
+    let version_name_regex = Regex::new(VERSION_NAME_REGEX)?;
+    let version_code_regex = Regex::new(VERSION_CODE_REGEX)?;
+
+    let content = version_name_regex.replace(&content, |caps: &Captures| {
+        format!("{}{}{}", &caps[1], next_version, &caps[3])
+    });
+
+    let next_version_code: u64 = (version_code_regex
+        .captures(&content)
+        .ok_or_else(|| anyhow!("Couldn't find version code"))?[2]
+        .parse::<u64>()?)
+        + 1;
+
+    let content = version_code_regex.replace(&content, |caps: &Captures| {
+        format!("{}{}", &caps[1], next_version_code)
+    });
+
+    let mut output = File::create(app_gradle_file)?;
+    output.write_all(content.as_bytes())?;
+
+    Ok(())
 }

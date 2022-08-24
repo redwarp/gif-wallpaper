7c7a6caaf7e54ecbe345b2c80541e47cb40b4be9
Benoit Vermont <redwarp@gmail.com>
2022-08-24 12:38:56 +0200

    refactor: Delete more useless code

--- app/src/main/java/net/redwarp/gifwallpaper/util/ColorUtils.kt
+++ app/src/main/java/net/redwarp/gifwallpaper/util/ColorUtils.kt
@@ -15,22 +15,8 @@
  */
 package net.redwarp.gifwallpaper.util
 
-import android.content.Context
-import android.graphics.Color
-import androidx.annotation.AttrRes
-import androidx.annotation.ColorInt
-import androidx.core.content.res.use
 import androidx.core.graphics.ColorUtils
 
 fun Int.isDark(): Boolean {
     return ColorUtils.calculateLuminance(this) < 0.5
-}
-
-@ColorInt
-fun Context.themeColor(
-    @AttrRes themeAttrId: Int
-): Int {
-    return obtainStyledAttributes(intArrayOf(themeAttrId)).use {
-        it.getColor(0, Color.MAGENTA)
-    }
 }
